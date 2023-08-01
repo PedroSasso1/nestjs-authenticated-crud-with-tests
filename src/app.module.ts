707c6e14533a8180e2e558f8c5894f00b1193c17
pedrosasso1 <pedrosassom@outlook.com>
2023-08-01 00:15:49 -0300

    chore: start user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DespesasModule } from './despesas/despesas.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DespesasModule],
+  imports: [DespesasModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })

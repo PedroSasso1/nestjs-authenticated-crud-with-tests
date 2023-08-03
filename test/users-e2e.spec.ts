07d80b1cba0b852d06c8b2250249466412c3d783
pedrosasso1 <pedrosassom@outlook.com>
2023-08-03 06:47:48 -0300

    test: users e2e

--- test/users-e2e.spec.ts
+++ test/users-e2e.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { isUUID } from 'class-validator';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -24,5 +25,48 @@ describe('AppController (e2e)', () => {
 
   it('should', () => {
     expect(true).toBeTruthy();
+  });
+
+  it('should create user', async () => {
+    const {
+      body: { id },
+    } = await request(app.getHttpServer())
+      .post('/users')
+      .send({ email: 'user@email.com', password: '123456' })
+      .expect(201);
+
+    expect(isUUID(id, 4)).toBeTruthy();
+  });
+
+  it('should get user', async () => {
+    const email = 'user@email.com';
+    const password = '123456';
+    const {
+      body: { id },
+    } = await request(app.getHttpServer())
+      .post('/users')
+      .send({ email, password })
+      .expect(201);
+
+    await request(app.getHttpServer())
+      .get(`/users/${id}`)
+      .send({ email, password })
+      .expect(200, { id, email });
+  });
+
+  it('should get users', async () => {
+    const email = 'user@email.com';
+    const password = '123456';
+    const {
+      body: { id },
+    } = await request(app.getHttpServer())
+      .post('/users')
+      .send({ email, password })
+      .expect(201);
+
+    await request(app.getHttpServer())
+      .get(`/users`)
+      .send({ email, password })
+      .expect(200, [{ id, email }]);
   });
 });
